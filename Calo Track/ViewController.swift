982d070d5a999cfa13b646b0faa3cce74025b1e0
Sushen Satturu <ssat0007@student.monash.edu>
2022-04-19 01:59:12 +1000

    Initial Commit

---
+++ Calo Track/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calo Track
+//
+//  Created by Sushen Satturu on 19/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
